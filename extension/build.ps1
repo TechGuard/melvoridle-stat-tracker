5683f6399b7c3fa72d80c96436fab4fd7afa1296
Robin van Nunen <robin@vnunen.nl>
2020-04-17 19:50:21 +0200

    Update extension. App is no longer bundled with the extension, it loads the app from github directly for faster updates.

--- extension/build.ps1
+++ extension/build.ps1
@@ -1,4 +1,3 @@
-$AppDir = "../app"
 $InputDir = "./src"
 $OutputDir = "./dist"
 
@@ -7,36 +6,13 @@ Write-Output "1. Prepare distribution directory ...`n"
 if (Test-Path $OutputDir) {
     Remove-Item $OutputDir -Recurse
 }
-if (!(Copy-Item $InputDir $OutputDir -Recurse -PassThru)) {
-    throw "Failed to create output directory."
-}
+New-Item -Path $OutputDir -ItemType "directory"
 
-$Match = Select-String -Path "$AppDir/package.json" -Pattern '"version": "([^"]+)"'
-if ($Match.Matches.Length -eq 0) {
-    throw "Failed to find app version."
-}
-
-$AppVersion = $Match.Matches.Groups[1].Value
-Write-Output "App Version: $AppVersion"
-
-(Get-Content "$OutputDir/manifest.json") -replace "{VERSION_NUMBER}", $AppVersion | Out-File "$OutputDir/manifest.json" -Encoding "UTF8"
-
-Write-Output "`n2. Building app ..."
-Push-Location $AppDir
-npm run build
-Pop-Location
-
-$DestPath = "$OutputDir/app.bundle.js"
-if (Test-Path $DestPath) {
-    Remove-Item $DestPath
-}
-Move-Item "$AppDir/dist/app.bundle.js" $DestPath
-
-Write-Output "`n3. Zipping extension ..."
+Write-Output "`n2. Zipping extension ..."
 $DestPath = "$OutputDir/extension.zip"
 if (Test-Path $DestPath) {
     Remove-Item $DestPath
 }
-Compress-Archive -Path "$OutputDir/*" -DestinationPath $DestPath
+Compress-Archive -Path "$InputDir/*" -DestinationPath $DestPath
 
 Write-Output "`nCompleted!`n"
--- extension/src/inject.js
+++ extension/src/inject.js
@@ -1,4 +1,4 @@
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = chrome.extension.getURL('app.bundle.js');
+script.src = 'https://techguard.github.io/melvoridle-stat-tracker/app/dist/app.bundle.js';
 document.head.appendChild(script);
