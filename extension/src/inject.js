6f86b937e535552d81fbda92315cabb81c17767b
Robin van Nunen <robin@vnunen.nl>
2020-04-22 22:47:28 +0200

    Remove everything except for dist folder

--- extension/src/inject.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://techguard.github.io/melvoridle-stat-tracker/app/dist/app.bundle.js';
-document.head.appendChild(script);
